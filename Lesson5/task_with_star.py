51d94d750599a244537f0a63e43df541f6f84c6e
tsched-mikro <mikrovatt@yandex.ru>
2023-06-16 14:57:25 +0300

    Lesson5

---
+++ Lesson5/task_with_star.py
@@ -0,0 +1,45 @@
+# Напишите функцию to_roman, которая преобразуют арабское число (val) в римское (roman_str).
+#
+# Современные римские цифры записываются, выражая каждую цифру отдельно,
+# начиная с самой левой цифры и пропуская цифру со значением нуля.
+# Римскими цифрами 1990 отображается: 1000=М, 900=СМ, 90=ХС; в результате MCMXC.
+# 2023 записывается как 2000=MM, 20=XX, 3=III; или MMXXIII.
+# В 1666 используется каждый римский символ в порядке убывания: MDCLXVI.
+#
+# Например (Ввод --> Вывод) :
+# 2008 --> MMVIII
+
+
+def to_roman(val):
+    # Здесь нужно написать код
+    roman_str = ''
+    code = {1: {1: 'I', 2: 'II', 3:'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9:'IX'},
+            10: {1: 'X', 2: 'XX', 3: 'XXX', 4: 'XL', 5: 'L', 6: 'LX', 7: 'LXX', 8: 'LXXX', 9: 'XC'},
+            100: {1: 'C', 2: 'CC', 3: 'CCC', 4: 'CD', 5: 'D', 6: 'DC', 7: 'DCC', 8: 'DCCC', 9: 'CM'},
+            1000: {1: 'M', 2: 'MM', 3: 'MMM'}
+            }
+    dict_val = []
+    index = 0
+    while val != 0:
+        dict_val.insert(0, (10**index, val % 10))
+        val = val // 10
+        index += 1
+    for k, v in dict_val:
+        if v != 0:
+            roman_str += code[k][v]
+    return roman_str
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+data = [1133, 2224, 1938, 1817, 2505, 391, 3743, 1634, 699, 1666, 1494, 1444]
+
+test_data = [
+    "MCXXXIII", "MMCCXXIV", "MCMXXXVIII", "MDCCCXVII", "MMDV", "CCCXCI", 'MMMDCCXLIII', 'MDCXXXIV', 'DCXCIX', 'MDCLXVI',
+    'MCDXCIV', 'MCDXLIV']
+
+
+for i, d in enumerate(data):
+    assert to_roman(d) == test_data[i], f'С набором {d} есть ошибка, не проходит проверку'
+    print(f'Тестовый набор {d} прошёл проверку')
+print('Всё ок')
