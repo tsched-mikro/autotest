50a0e91539c65592b682aee0f25aaea4d07b01ec
tsched-mikro <mikrovatt@yandex.ru>
2023-06-19 09:42:57 +0300

    Lesson8

---
+++ Lesson8/task_2.py
@@ -0,0 +1,60 @@
+# Напишите класс Trigon, для инициализации передаётся неизвестное кол-во атрибутов
+
+# В классе при инициализации происходит проверка на корректность переданных данных и генерируются следующие исключения:
+
+# 1) Если хотя бы одна сторона передана не числом,
+# то падаем с TypeError и текстом 'Стороны должны быть числами'
+
+# 2) Если хотя бы одна сторона передана нулем или отрицательным числом,
+# то падаем с ValueError и текстом 'Стороны должны быть положительными'
+
+# 3) Если не соблюдается неравество треугольника,
+# то Exception и текст "Не треугольник"
+
+# 4) Если передано не 3 аргумента, то IndexError "Передано {n} аргументов, а ожидается 3", где n - кол-во аргументов
+
+import unittest  # Не удалять
+
+
+# Здесь пишем код
+class Trigon:
+    def __init__(self, *args):
+        if len(args) != 3:
+            text = f'Передано {len(args)} аргументов, а ожидается 3'
+            raise IndexError(text)
+        if not all((isinstance(el, int) for el in args)):
+            raise TypeError('Стороны должны быть числами')
+        if not all((el > 0 for el in args)):
+            raise ValueError('Стороны должны быть положительными')
+        self.a, self.b, self.c = args
+        triangle_flag = all((self.a + self.b > self.c, self.a + self.c > self.b, self.b + self.c > self.a,
+                             self.a > 0, self.b > 0, self.c > 0))
+        if not triangle_flag:
+            raise Exception('Не треугольник')
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test(self):
+        data = [(3, '7', 5), (-3, 7, 5), (2, 5, 2), (3, 4, 5, 6), (3, 4), (3, 4, 5)]
+
+        test_data = [('Стороны должны быть числами', 'TypeError'),
+                     ('Стороны должны быть положительными', 'ValueError'),
+                     ("Не треугольник", 'Exception'),
+                     ("Передано 4 аргументов, а ожидается 3", 'IndexError'),
+                     ("Передано 2 аргументов, а ожидается 3", 'IndexError'),
+                     0]
+        for i, d in enumerate(data):
+            try:
+                Trigon(*data[i])
+            except Exception as e:
+                print(e.args[0])
+                assert e.args[0] == test_data[i][0], 'Исключение имеет неправильный текст'
+                assert type(e).__name__ == test_data[i][1], 'У исключения неправильный тип'
+
+        print('Всё ок')
+
+
+if __name__ == '__main__':
+    unittest.main()
